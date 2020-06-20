ef06d228d3d10e399258b5a992dcaac0b9f51541
anjitzero <anjitpariyar@gmail.com>
2020-06-20 11:15:30 +0545

    Updates

---
+++ precache-manifest.2ba42866fec9b17da93945077b3b4a8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfdeaa898d78d7fa9c35860b3ab503a4",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "d54f0c4bef4b48140d47",
+    "url": "/covid-tracker/static/css/main.e5d948bc.chunk.css"
+  },
+  {
+    "revision": "d58fdd6cc5af9e765c3c",
+    "url": "/covid-tracker/static/js/2.96dcc590.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.96dcc590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d54f0c4bef4b48140d47",
+    "url": "/covid-tracker/static/js/main.d3c1112e.chunk.js"
+  },
+  {
+    "revision": "c3c83fb68889115e71aa",
+    "url": "/covid-tracker/static/js/runtime-main.7dd44adf.js"
+  }
+]);
